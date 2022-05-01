7cf4b7abf2f43fd530399703008f4456514e1919
prerak-jain <prerakjain1007@gmail.com>
2022-05-01 21:17:29 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const navSlide = function () {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${
+                    index / 20
+                }s`;
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
+};
+
+window.addEventListener('load', navSlide);
